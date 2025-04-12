66378f9c15faf29fccc26bf47aa31d50209b6e72
Ceren Çelikkol <191454758+Ceren-Celikkol@users.noreply.github.com>
2025-04-12 18:35:06 +0300

    Add files via upload

---
+++ proje-js9793p/javascript.js
@@ -0,0 +1,9 @@
+function sayiArttir() {
+   let sayi = document.getElementById("artacak");
+
+   let sayiDonustur = Number(sayi.innerHTML);
+
+   sayiDonustur ++;
+   sayi.innerHTML = sayiDonustur ++;
+
+}
